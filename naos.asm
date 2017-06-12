985bf7ce19ebb3496db8057904916e3e2691aa0f
MarcinJXXXX <marcinstarwars@gmail.com>
2017-06-12 15:44:48 +0200

    Update naos.asm

--- naos.asm
+++ naos.asm
@@ -20,14 +20,96 @@ mov bl, 0
 int 10h
 
 main:
-	call Load
+	;call Load
+	;call ClearScreen
+	;call LogIn
 	call ClearScreen
 mainLoop:
-	
+	mov ah, 2
+	; implement weed 4 spacer
+	int 10h
 	
 	jmp mainLoop
 
+LogIn:
+	mov ah, 2
+	mov bh, 0
+	mov dh, 10
+	mov dl, 35
+	int 10h
+	
+	mov si, unamemsg
+	call PrintString
+	
+	mov ah, 2
+	mov bh, 0
+	mov dh, 11
+	mov dl, 35
+	int 10h
+	
+	mov si, username	
+LogInLoop:
+	mov ah, 0
+	int 16h
+	
+	cmp al, 13
+	je LogInEnd
+	
+	mov bx, si
+	sub bx, username
+	cmp bx, 8
+	je LogInEnd
+	
+	mov [si], al 
+	inc si
+	
+	call PrintChar
+	
+	jmp LogInLoop
+LogInEnd:
+	ret
+
 Load:
+	mov ah, 2
+	mov bh, 0
+	mov dx, 0
+	int 10h
+	
+	mov ah, 9
+	mov al, ' '
+	mov bh, 0
+	mov bl, 0x0F
+	mov cx, 0x7D0
+	int 10h
+	
+	mov ah, 2
+	mov bh, 0
+	mov dh, 11
+	mov dl, 36
+	int 10h
+	
+	mov si, logoz
+	call PrintString
+	
+	mov ah, 0x86
+	mov cx, 0x1E
+	mov dx, 0x8480
+	int 15h
+	
+	mov ah, 2
+	mov bh, 0
+	mov dh, 11
+	mov dl, 36
+	int 10h
+	
+	mov si, logoz2
+	call PrintString
+	
+	mov ah, 0x86
+	mov cx, 0x1E
+	mov dx, 0x8480
+	int 15h
+	
 	call ClearScreen
 	
 	mov ah, 2
@@ -118,6 +200,15 @@ ClearScreen:
 	
 	mov ah, 2
 	mov bh, 0
+	mov dh, 24
+	mov dl, 0
+	int 10h
+	
+	mov si, username
+	call PrintString
+	
+	mov ah, 2
+	mov bh, 0
 	mov dh, 1
 	mov dl, 0
 	int 10h
@@ -143,7 +234,14 @@ PrintLoop:
 PrintEnd:
 	ret
 
-osName: db "Not A OS v0.666", 0
+osName: db "Weed 4 Spacer OS 2017", 0
+
+logoz: db "By MB", 0
+logoz2: db "& MJX", 0
+
+unamemsg: db "Username", 0
+username: db "        ", 0
+
 loadMsg: db "Loading...", 0
 loadProg: db 0
 
