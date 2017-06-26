4924d0bb7eb26143054d2882c68b8915512dce44
MarcinJXXXX <MarcinJXXXX@users.noreply.github.com>
2017-06-26 17:18:49 +0200

    Added things from branch "w4s experimental"

--- highscores.asm
+++ highscores.asm
@@ -1,4 +1,7 @@
-LoadHighScore:
+LoadHighScores:
+	jmp jmp_over
+	temp_username: db "         ", 0
+jmp_over:
 	pusha
 	mov ah, 2
 	mov al, 1
@@ -8,21 +11,45 @@ LoadHighScore:
 	mov dl, 0x80
 	mov bx, buffer__
 	int 13h
-	mov ah, [buffer__]
-	mov [def], ah
-	mov ah, [buffer__+1]
-	mov [def+1], ah
-	mov si, def
-	call PrintString
+	mov ax, [buffer__]
+	mov [def], ax
+	mov dx, 0
+.loop:
+	cmp dx, 8
+	jge .end_loop
+	inc dx
+	mov si, dx
+	add si, 1
+	mov ah, [buffer__+si]
+	sub si, 2
+	mov [temp_username+si], ah
+	jmp .loop 
+.end_loop:
+	;mov si, temp_username
+	;call PrintString
+	;mov si, def
+	;call PrintString
 	popa
 	ret
 	
 WriteHighScores:
 	pusha
-	mov ah, [score]
-	mov [buffer__], ah
-	mov ah, [score+1]
-	mov [buffer__+1], ah
+
+	mov ax, [score]
+	mov [buffer__], ax
+	mov dx, 0
+.loop:
+	cmp dx, 8
+	jge .end_loop
+	inc dx
+	mov si, dx
+	dec si
+	mov ah, [username+si]
+	inc si
+	inc si
+	mov [buffer__+si], ah
+	jmp .loop 
+.end_loop:
 	mov ah, 3
 	mov al, 1
 	mov ch, 0
