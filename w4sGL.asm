30ebbeafa1fb33ef338a032f5a690a3af7a8ac36
MarcinJXXXX <MarcinJXXXX@users.noreply.github.com>
2017-07-06 20:59:27 +0200

    Finish drawRectangle()
    drawRectangle
    =================================================
    Parameters:
    -'ax' containing 'sp' from moment before call
    on stack (in order you push them):
    -start position X
    -start position Y
    -size X
    -size Y
    -color
    =================================================
    I know "parameters list says 'sizeY' and 'sizeX', but in code it's 'endX' and 'endY'". I don't care! I'm lazy, I started writing this code with 'endX' and 'endY', so there's no reason to change it.

--- w4sGL.asm
+++ w4sGL.asm
@@ -6,6 +6,7 @@ drawPixel:
   mov ah, 0x0c
   mov bl, 0
   int 10h
+  
   ret
   
 ;Initialize video mode
@@ -22,55 +23,92 @@ setupVideoMode:
 ;push [endX]
 ;push [endY]
 ;push [color]
-;UNFINISHED
+;sp in ax
+;FINISHED(???)
 drawRectangle:
-  pop ax
-  mov [color], ax
-  pop ax
-  mov [endY], ax
-  pop ax
-  mov [endX], ax
-  pop ax
-  mov [startY], ax
-  pop ax
-  mov [startX], ax
-  xor ax, ax
-  .loop:
-    push ax
-    mov ax, [looper]
-    add ax, [startX]
-    cmp ax, [endX]
-    jge .end
-    mov cx, ax
- .loop2:
-    push ax
-    mov ax, [looper2]
-    add ax, [startY]
-    cmp ax, [endY]
-    jge .endloop2
-    mov dx, ax
-    mov al, [color]
-    call drawPixel
-    pop ax
-    jmp .loop2
- .endloop2:
-    pop ax
-    mov ax, [looper]
-    inc ax
-    mov [looper], ax
-    pop ax
-    jmp .loop
-  .end:
-  
-  pop ax
-  ret
+pop word [address] ;pop address to return to, that is pushed by 'call' and later used by 'ret'
+				   ;since this code changes 'sp', it's safer to store this address :) (like, it might get lost somewhere in memory)
+				   ;(even restoring 'sp' doesn't help enough)
+mov bx, sp
+mov [oldsp], bx
+xor bx, bx
+mov sp, ax ;restore stack
+
+
+pop word [color]
+pop word [endY]
+pop word [endX]
+pop word [startY]
+pop word [startX]
+
+
+;mov ax, [startX]
+;cmp ax, [endX]
+;jge .error_X
+;xor ax, ax
+;mov ax, [startY]
+;cmp ax, [endY]
+;jge .error_Y
+
+mov cx, 0
+
+.loop:
+  cmp cx, [endX]
+  je .done
+  mov bx, 0
+.loop2:
+	cmp bx, [endY]
+  je .loop2Done
+		   
+	pusha
+  mov al, [color]
+	mov dx, bx
+	add dx, [startX]
+	add cx, [startY]
+	call drawPixel
+	popa	   
+  inc bx
+  jmp .loop2
+.loop2Done:
+  inc ecx
+  jmp .loop
+.done:	
+	mov bx, [oldsp]
+	mov sp, bx
+	xor bx, bx
+
+	push word 0
+  jmp [address]
+;.error_X:
+;	mov cx, [startX]
+;	mov dx, [startY]
+;	mov al, 0x4
+;	call drawPixel
+;	mov bx, [oldsp]
+;	mov sp, bx
+;	xor bx, bx
+;	push word 1
+;	jmp [address]
+;.error_Y:
+;	mov cx, [startX]
+;	mov dx, [startY]
+;	mov al, 0xc
+;	call drawPixel
+;	mov bx, [oldsp]
+;	mov sp, bx
+;	xor bx, bx
+;	push word 2
+;	jmp [address]
+
+oldsp: dw 0
 startX: dw 0
 startY: dw 0
 endX: dw 0
 endY: dw 0
-looper: db 0
-looper2: db 0
-color: db 0
+looper: dw 0
+looper2: dw 0
+color: dw 0
+address: dw 0
 ;VESA-Compliant video mode 1024x768
 ;I'm not sure what's it, but Wikipedia says it makes graphics HD with 256 colors
 ;Well, that's not RGBA, but still, it's huge improvement over text mode...
